b2b904933bf9b07b27641b42675de79ee03c94ce
Zinno <4070806@vtsd.com>
2020-03-11 08:24:09 -0400

    jij

--- Shape.java
+++ Shape.java
@@ -7,9 +7,15 @@ import java.awt.*;
 public abstract interface Shape
 {
 	public abstract void drawShape(Graphics g);
-
+		Shape 1 = new Shape1Square();
+		Shape 2 = new Shape2Triangle();
+		Shape 3 = new Shape3Octagon();
+		Shape 4 = new Shape4Circle();
+			Shape 1.drawShape(g);
+			Shape 2.drawShape(g);
+			Shape 3.drawShape(g);
+			Shape 4.drawShape(g);
 	// public abstract void displayName(Graphics g);
 
 	// public abstract void displayNumSides(Graphics g);
 }
-
--- Shape1Square.java
+++ Shape1Square.java
@@ -8,6 +8,7 @@ public class Shape1Square implements Shape
 {
    public void drawShape(Graphics g)
    {
+     g.setColor(new Color(0,0,0));
+     g.fillRect(125,110,175,160);
    }
 }
-
